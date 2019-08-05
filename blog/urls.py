b770f4832871c1794ff20081ee4c00dee2d28f2b
Yahta20 <Yahta2v0more@gmail.com>
2019-08-06 03:28:47 +0400

    post midify, delete, update,

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog'),
+    path('', views.ShowPostView.as_view(), name='blog'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='blog_detail'),
+    path('post/<int:pk>/update', views.UpdatePostView.as_view(), name='blog_update'),
+    path('post/<int:pk>/delete', views.DeletePostView.as_view(), name='blog_delete'),
+    path('post/add/', views.CreatePostView.as_view(), name='blog_add'),
     path('contacts/', views.contacts, name='contacts'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    )
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 def home (request):
@@ -8,6 +16,61 @@ def home (request):
     }
     return render (request, 'blog/home.html',data)
 
+class ShowPostView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name  = 'news'
+    ordering = ['-date']
+
+    def get_context_data(self, **kwards):
+        ctx = super(ShowPostView, self).get_context_data(**kwards)
+        ctx ['title'] = 'Main page'
+        return ctx
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwards):
+        ctx = super(PostDetailView, self).get_context_data(**kwards)
+        ctx['title'] = Post.objects.filter(pk=self.kwargs['pk']).first()
+        return ctx
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = [
+        'title',
+        'text'
+    ]
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+
+class UpdatePostView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = [
+        'title',
+        'text'
+    ]
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        news = self.get_object()
+        if self.request.user == news.autor :
+            return True
+        return False
+
+class DeletePostView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+
+    success_url = '/'
+
+    def test_func(self):
+        news = self.get_object()
+        if self.request.user == news.autor :
+            return True
+        return False
 
 def contacts (request):
     return render (request, 'blog/contacts.html',{'title':'Our contacts'})
